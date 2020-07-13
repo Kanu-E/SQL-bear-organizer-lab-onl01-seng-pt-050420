8a86655c0b1b0a77353c186b739591830e06a481
Emeka Kanu <chukwuemekakanu@gmail.com>
2020-07-13 00:05:26 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 23, "female", "brown", "cool", "TRUE");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 23, "female", "brown", "cool", "TRUE");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 23, "female", "brown", "cool", "TRUE");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 23, "female", "brown", "cool", "TRUE");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 23, "female", "brown", "cool", "TRUE");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 23, "female", "brown", "cool", "TRUE");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 23, "female", "brown", "cool", "TRUE");
+INSERT INTO bears ( name, age, gender, color, temperament, alive)
+VALUES (NULL, 23, "female", "brown", "cool", "TRUE");
